9ad4dcdc551c7c32ed736ff7ace39d6da3e9c158
Corey Ladovsky <coreyskymusic@gmail.com>
2019-01-07 12:10:05 -0500

    warm up complete

---
+++ src/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+      Sign UP!
+      </div>
+    )
+  }
+}
+
+export default SignUp;
